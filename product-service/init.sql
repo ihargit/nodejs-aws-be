5b8742f5c7fabbb0c0a87dfb5ce54e9b67565bb0
Ihar Kharashun <ihar.kharashun@gmail.com>
2020-11-13 19:43:30 +0300

    feat: update POST product method

--- product-service/init.sql
+++ product-service/init.sql
@@ -9,40 +9,19 @@ CREATE TABLE IF NOT EXISTS products (
 
 CREATE TABLE IF NOT EXISTS stocks (
 	id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-	product_id uuid references products(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    count INTEGER DEFAULT 0
+	product_id uuid references products(id) ON DELETE CASCADE ON UPDATE cascade UNIQUE,
+    count INTEGER CHECK (count >= 0) DEFAULT 0
 );
 
-CREATE OR REPLACE FUNCTION init_stocks()
-	RETURNS trigger
-	LANGUAGE PLPGSQL
-    AS 
-	$$
-	BEGIN
-		INSERT INTO stocks(product_id)
-		VALUES(NEW.id);
-		RETURN NEW;
-	END;
-	$$;
-
-DROP TRIGGER IF EXISTS init_stocks ON products;
-
--- insert row to 'stocks' table after row is inserted to 'products' table
-CREATE TRIGGER init_stocks
-  AFTER INSERT
-  ON products
-  FOR EACH ROW
-  EXECUTE FUNCTION init_stocks();
-
--- populate 'products' and (by triggering 'init_stocks' function) 'stocks' tables
-INSERT INTO products(title, description, price)
-VALUES('Bunny', 'Bunny toy', 1);
-
-INSERT INTO products(title, description, price)
-VALUES('Bear', 'Bear toy', 2);
-
-INSERT INTO products(title, description, price)
-VALUES('Wolf', 'Wolf toy', 3);
-
--- set random 'count' to 'stocks' (1 > count < 11)
-UPDATE stocks SET count=floor(random() * 10 + 1)::int
+with rows as (
+	insert into products(title, description, price)
+values('Bear', 'Bear toy', 1) returning *
+)
+insert into stocks(count, product_id)
+values(2, (select id from rows))
+returning
+	(select id from rows),
+	(select title from rows),
+	(select description from rows),
+	(select price from rows),
+	count;
